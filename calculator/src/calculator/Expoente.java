44c549cef83096eb3ca8d639e7e8633f9e22a1b8
Izabelle Lisboa <izabellelimalisboa10@gmail.com>
2022-09-16 11:53:04 +0100

    lerdados e expoente

--- calculator/src/calculator/Expoente.java
+++ calculator/src/calculator/Expoente.java
@@ -5,6 +5,9 @@ package calculator;
 class Expoente {
 
     static void potencia() {
+        LeDados r = new LeDados();
+        r.lerValores();
+        System.out.println("Potência: "+ Math.pow(r.valor1, r.valor2));
     }
     
 }
