130cd6f191291bc475c78ee319f973398025ca4f
Izabelle Lisboa <izabellelimalisboa10@gmail.com>
2022-09-16 13:03:11 +0100

    IMC

---
+++ calculator/src/calculator/IMC.java
@@ -0,0 +1,29 @@
+
+package calculator;
+
+
+class IMC {
+
+    static void calculaIMC() {
+        LeDados r = new LeDados();
+        r.lerValores();
+        r.IMC = r.valor1 / (r.valor2 * r.valor2);
+        System.out.println ("IMC:"+ r.IMC );
+        
+        if (r.IMC <= 18.5){
+        System.out.println ("IMC: Abaixo do peso" );
+        }
+        else if ((r.IMC > 25.0) && (r.IMC<= 30.0)){
+        System.out.println ("IMC: Peso normal" ); 
+        } 
+        else if (r.IMC > 30.0 && (r.IMC<= 34.9)) {
+        System.out.println ("IMC: Obesidade Grau I" ); 
+        } 
+        else if (r.IMC > 35.0 && (r.IMC<= 39.9)) {
+        System.out.println ("IMC: Obesidade Grau II" ); 
+        }
+        else if (r.IMC<= 40.0) {
+        System.out.println ("IMC: Obesidade Grau II" ); 
+        }
+    }
+}
--- calculator/src/calculator/LeDados.java
+++ calculator/src/calculator/LeDados.java
@@ -3,13 +3,17 @@ package calculator;
 
 
 public class LeDados {
-     public float valor1, valor2;
+     public float valor1, valor2,IMC;
     
     public void lerValores(){
         System.out.println("Insira um valor");
         valor1 = Calculator.ler.nextFloat();
         System.out.println("Insira outro valor");
         valor2 = Calculator.ler.nextFloat();
+        /*System.out.println("Insira peso em kg");
+        peso = Calculator.ler.nextFloat();
+        System.out.println("Insira uma altura em metros");
+        altura = Calculator.ler.nextFloat();*/
     }
 } 
     
